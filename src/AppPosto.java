6a717b78b9f4a31acc01f51266ecb72d6bd1503f
rodrism <santiagodemenezes@gmail.com>
2020-06-19 17:17:48 -0300

    Novos arquivos

---
+++ src/AppPosto.java
@@ -0,0 +1,8 @@
+
+public class AppPosto {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
--- src/BombaCombustivel.java
+++ src/BombaCombustivel.java
@@ -1,4 +1,35 @@
 
 public class BombaCombustivel {
-
+	private String combustivel;
+	private float valorLitro;
+	private float qtdLitros;
+	private float valorTotal;
+	
+	
+	public String getCombustivel() {
+		return combustivel;
+	}
+	public void setCombustivel(String combustivel) {
+		this.combustivel = combustivel;
+	}
+	public float getValorLitro() {
+		return valorLitro;
+	}
+	public void setValorLitro(float valorLitro) {
+		this.valorLitro = valorLitro;
+	}
+	public float getQtdLitros() {
+		return qtdLitros;
+	}
+	public void setQtdLitros(float qtdLitros) {
+		this.qtdLitros = qtdLitros;
+	}
+	public float getValorTotal() {
+		return valorTotal;
+	}
+	public void setValorTotal(float valorTotal) {
+		this.valorTotal = valorTotal;
+	}
+	
+	
 }
